09cd961f14fb43bb12d3fa344c17b2248fe95fa6
Rokiora <85311450+Rokiora@users.noreply.github.com>
2024-02-21 14:46:33 -0800

    added main_menu module
    main_menu function in user_input.py will execute control flow for user options.

--- Crap/main.py
+++ Crap/main.py
@@ -1,5 +1,5 @@
 from database import Database
-from user_input import client_info
+from user_input import main_menu
 
 
 def main():
@@ -13,14 +13,15 @@ def main():
 
 # main loop
     while True: # may need to change this loop, perhaps replaced with a function? for simplicity? function should handl options (add client, update client, etc.)
-        client_data = client_info().__dict__ # store client_info function return from user_input.py in client_data
-        db.insert_client(client_data) # pass client data as dictionary into insert_client method from database.py
-        user = input('would you like to add another client?') # ask user to add another
+        main_menu()
+       # client_data = client_info().__dict__ # store client_info function return from user_input.py in client_data
+       # db.insert_client(client_data) # pass client data as dictionary into insert_client method from database.py
+       # user = input('would you like to add another client?') # ask user to add another
 
 # exit contingency
-        if user.lower() == 'no': 
-            print('we are done here')
-            break
+       # if user.lower() == 'no': 
+       #     print('we are done here')
+       #     break
 
 if __name__ == "__main__":
     main()
--- Crap/user_input.py
+++ Crap/user_input.py
@@ -2,9 +2,33 @@ from client import Client
 import datetime
 from datetime import timedelta
 
+
+# user options menu will go here
+def main_menu():
+     print('what would you like to do?')
+     user = input(
+          """
+1. add a new client
+2. update client data
+3. check upcoming reports
+4. delete client
+""")
+     if int(user) == 1:
+          client_add()
+     elif int(user) == 2:
+          print('you chose 2')
+     elif int(user) == 3:
+          print('you chose 3')
+     elif int(user) == 4:
+          print('you chose 4')
+          
+
+
+
+# client addition module
 #strptime parses string into datetime object
 #strftime converts object to desired string format
-def client_info():
+def client_add():
     uname = input('Enter client pseudonym: ')
     ustart = datetime.datetime.strptime(input('Enter contract start date (dd mm yy): '), "%d %m %y").date().strftime("%d-%m-%y")
     uend = datetime.datetime.strptime(input('Enter contract end date (dd mm yy): '), "%d %m %y").date().strftime("%d-%m-%y")
@@ -27,6 +51,10 @@ def client_info():
 
     return Client(name=uname, start=ustart, end=uend, codes=ucodes, vine=uvine, report_due=ureport_due, clinint=uclinint, submitted=usubmitted)
 
-# user options menu will go here
 
 # client update will go here
+
+# check upcoming reports
+
+
+# delete client
